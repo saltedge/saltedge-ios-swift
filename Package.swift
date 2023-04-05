517ddec1df33ec232afb238a48f1433bce2264dd
Erwan <erwan@bitsmedia.com>
2023-04-05 15:15:55 +0800

    migrate package swift file to version 5.6

--- Package.swift
+++ Package.swift
@@ -1,1 +1,16 @@
-.package(url: "https://github.com/Dobin-Pte-Ltd/saltedge-ios-swift.git", .branch("master"))
+// swift-tools-version:5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "Saltedge-iOS-Swift",
+    products: [
+       .library(name: "SaltEdge", targets: ["SaltEdge"])
+   ],
+   targets: [
+       .target(
+           name: "SaltEdge",
+           path: "SaltEdge"
+       )
+   ]
+)
